90628d41cb4f913dfa2d8e8abac1009a804c4a23
Georgia <GeorgiaSquared@gmail.com>
2020-01-07 22:42:51 -0500

    completed exercises 0-9, Learned lots of stuff. All good stuff. Still don't know what to write on commit messages and deeply regret not listening closely enough when Nik explained them.

---
+++ ex3.py
@@ -0,0 +1,32 @@
+"""
+Exercise 3
+Numbers and Math
+1/7/20
+"""
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# % is called a modulus, not a percent.
+# Another way to say it is “X divided by Y with J remaining.”
+# For example, “100 divided by 16 with 4 remaining.” The result of % is the J part, or the remaining part.
+
+print("Is it true that 3 + 2 < 5 - 77?")
+
+print(3 + 2 < 5 - 77)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 77?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
