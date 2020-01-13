5f5b5186f12caa4972ea445b67831202ce7e9868
georgiasquared <GeorgiaSquared@gmail.com>
2020-01-13 15:43:50 -0500

    created first mini project

---
+++ NumberGuess.py
@@ -0,0 +1,7 @@
+"""Write a program where the computer randomly generates a number between 0 and 20.
+The user needs to guess what the number is.
+If the user guesses wrong, tell them their guess is either too high, or too low.
+This will get you started with the random library if you haven't already used it."""
+
+import random
+
