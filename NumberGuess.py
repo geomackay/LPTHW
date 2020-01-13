447af0dca5416d431364c77b2ae0037c7cc27424
Georgia <GeorgiaSquared@gmail.com>
2020-01-13 16:05:52 -0500

    working number guessing game through console input

--- NumberGuess.py
+++ NumberGuess.py
@@ -5,3 +5,32 @@ This will get you started with the random library if you haven't already used it
 
 import random
 
+
+def guessing_game():
+
+    # Create machines number
+    num = random.randint(0, 20)
+
+    def user_attempt():
+        # Prompt user for guess
+        guess = int(input("Enter guess: "))
+        checks_n_balances(guess)
+
+    def checks_n_balances(guess):
+        if num == guess:
+            win_condition()
+        if num > guess:
+            print("Too Low!")
+            user_attempt()
+        if num < guess:
+            print("Too High!")
+            user_attempt()
+
+    def win_condition():
+        print("You Win!")
+
+    print("Guess the number between 0 and 20")
+    user_attempt()
+
+
+guessing_game()
